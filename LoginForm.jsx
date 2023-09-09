716a18d0a27b1cdf1bac5a36bf7457c12dfc2e24
GayathreeBala <126935532+GayathreeBala@users.noreply.github.com>
2023-09-09 18:25:24 +0530

    Create LoginForm.jsx

---
+++ LoginForm.jsx
@@ -0,0 +1,62 @@
+// LoginForm.js (React Component)
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // In a real application, you would send this data to the server for login validation.
+    console.log('Login data:', this.state);
+    // Clear the form fields after submission
+    this.setState({
+      email: '',
+      password: '',
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
